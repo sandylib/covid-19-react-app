e43c34da86c4dc057561ab0854945bb93aedd314
zhangsa <sandy.zhang@tabcorp.com.au>
2020-04-22 12:44:54 +1000

    Updates

---
+++ precache-manifest.9736ecff851767edfa0270d0f71cadcd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d331282d94393bbf3a6b4a78a165f38",
+    "url": "/covid-19-react-app/index.html"
+  },
+  {
+    "revision": "980d1f5f929387447758",
+    "url": "/covid-19-react-app/static/css/main.cd3aed2c.chunk.css"
+  },
+  {
+    "revision": "827d8e134763d5e32852",
+    "url": "/covid-19-react-app/static/js/2.9b64b038.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-react-app/static/js/2.9b64b038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980d1f5f929387447758",
+    "url": "/covid-19-react-app/static/js/main.a698dbb2.chunk.js"
+  },
+  {
+    "revision": "3beb6a29753196701e33",
+    "url": "/covid-19-react-app/static/js/runtime-main.8b5fa6e1.js"
+  },
+  {
+    "revision": "861241ac148fce7e3cea40d53fc2682e",
+    "url": "/covid-19-react-app/static/media/back.861241ac.svg"
+  },
+  {
+    "revision": "da888ab4a72210f7036e54d2ad1e2e74",
+    "url": "/covid-19-react-app/static/media/covid19.da888ab4.jpeg"
+  }
+]);
